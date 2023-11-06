a411fd4012bfa588dadd6d88eb128421ff5940a4
chiming wang <chimingwang0@gmail.com>
2023-11-06 17:51:23 -0500

    renamed folder

---
+++ 1.1.1 Getting Things Working/Sep06.java
@@ -0,0 +1,13 @@
+/*
+Chiming Wang
+9/07/2023
+AP Computer Science A
+Period 5 Weisswange
+Assignment 1
+*/
+
+public class Sep06 {   
+   public static void main (String[] args){
+      System.out.println("Hi Mr. Weisswange");
+   }
+}
